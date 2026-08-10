a9d42d1c4d93b9f5fb49e12ad9ee6096a318a63c
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-08-10 17:18:31 +0500

    fix: correct typo in createUserSchema field name

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,6 +3,7 @@ import express from "express"
 import {createUserSchema} from "@monorepo-form-builder-trpc/utils"
 
 const app = express()
+app.use(express.json())
 
 const PORT = 5000
 
@@ -12,6 +13,28 @@ app.get("/" , (req , res)=>{
     })
 });
 
+app.post("/users" , (req , res)=>{
+    const result = createUserSchema.safeParse(req.body);
+
+    if(!result.success){
+       const message = result.error.issues
+        .map((issue) => issue.message)
+        .join(", ")
+
+        return res.status(400).json({
+            success : false ,
+            message : message
+        })
+    }
+
+     console.log(result.data)
+
+        return res.json({
+            success : true ,
+            message : "user created"
+        })
+
+})
 app.listen(PORT , ()=>{
     console.log(`Server started at PORT : ${PORT}`)
 });
