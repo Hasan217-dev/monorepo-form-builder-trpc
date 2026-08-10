379378bf915c10c11b04b30dee1e97f0b663b4de
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-08-10 18:06:26 +0500

    fix: disable builds for unrs-resolver in pnpm workspace configuration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,11 @@
-import express from "express"
 
+import express from "express"
+import cors from "cors"
 import {createUserSchema} from "@monorepo-form-builder-trpc/utils"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const PORT = 5000
 
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,69 +1,75 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { createUserSchema } from "@monorepo-form-builder-trpc/utils";
+
+import type { FormHTMLAttributes, SubmitEvent } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [name, setUname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  async function handleSubmit(e: SubmitEvent) {
+    setError("")
+    setSuccess("")
+    e.preventDefault();
+
+    const result = createUserSchema.safeParse({
+      name,
+      email,
+      password,
+    });
+
+    if (!result.success) {
+      const message = result.error.issues
+        .map((issue) => issue.message)
+        .join(",");
+
+      setError(message);
+    }
+
+    try {
+      const response = await axios.post("http://localhost:5000/users" , result.data)
+      setSuccess("User created successfully")
+    } catch (error) {
+      setError("Some error ocurred")
+    }
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main>
+      <form onSubmit={handleSubmit} noValidate={true}>
+        <input
+          type="text"
+          placeholder="Hasan"
+          value={name}
+          onChange={(e) => setUname(e.target.value)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
+        <input
+          type="email"
+          placeholder="hasan@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p> {error} </p>}
+        {success && <p> {success} </p>}
+
+        <button type="submit">Submit</button>
+      </form>
+    </main>
   );
 }
