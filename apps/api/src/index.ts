2389746fc519b4e9ff8ed79e80fc38e0dc020b70
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-08-11 19:02:04 +0500

    feat: update dependencies in package.json and pnpm-lock.yaml for TRPC integration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,11 @@
 
 import express from "express"
 import cors from "cors"
-import {createUserSchema} from "@monorepo-form-builder-trpc/utils"
+
+import {createExpressMiddleware} from "@trpc/server/adapters/express"
+import {appRouter} from "@monorepo-form-builder-trpc/trpc"
+
+// import {createUserSchema} from "@monorepo-form-builder-trpc/utils"
 
 const app = express()
 app.use(express.json())
@@ -9,34 +13,40 @@ app.use(cors())
 
 const PORT = 5000
 
-app.get("/" , (req , res)=>{
-    return res.json({
-        message : "Hello Peers"
-    })
-});
+app.use("/trpc" , createExpressMiddleware({
+    router : appRouter
+}))
 
-app.post("/users" , (req , res)=>{
-    const result = createUserSchema.safeParse(req.body);
 
-    if(!result.success){
-       const message = result.error.issues
-        .map((issue) => issue.message)
-        .join(", ")
+// app.get("/" , (req , res)=>{
+//     return res.json({
+//         message : "Hello Peers"
+//     })
+// });
 
-        return res.status(400).json({
-            success : false ,
-            message : message
-        })
-    }
+// app.post("/users" , (req , res)=>{
+//     const result = createUserSchema.safeParse(req.body);
 
-     console.log(result.data)
+//     if(!result.success){
+//        const message = result.error.issues
+//         .map((issue) => issue.message)
+//         .join(", ")
 
-        return res.json({
-            success : true ,
-            message : "user created"
-        })
+//         return res.status(400).json({
+//             success : false ,
+//             message : message
+//         })
+//     }
 
-})
+//      console.log(result.data)
+
+//         return res.json({
+//             success : true ,
+//             message : "user created"
+//         })
+
+// })
+
 app.listen(PORT , ()=>{
     console.log(`Server started at PORT : ${PORT}`)
 });
